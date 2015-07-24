7f1a94b9d94daf95747789df3e4fb0e39cbaaaad
Taras Dolynyuk <trsdln@gmail.com>
2015-07-24 11:35:24 +0300

    More stubs + remove default user id

--- both/lib/app.js
+++ both/lib/app.js
@@ -8,9 +8,5 @@ App = {
       check(id, String);
       return /[0-9a-zA-Z]{17}/.test(id);
     })
-  },
-  //temporal constant instead of user id
-  temp: {
-    defaultUserId: 'abcdefglkmnoprst'
   }
 };
